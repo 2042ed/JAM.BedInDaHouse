5101dfd2e978429865957c00d2f85e8fb6bd4aa1
Stefano Cecere <stefano.cecere@krur.com>
2019-01-27 08:38:27 +0100

    alien at home

--- Assets/_game/_scripts/FollowManager.cs
+++ Assets/_game/_scripts/FollowManager.cs
@@ -24,15 +24,36 @@ namespace JAM
 
         public void AddToQueue(Transform GO)
         {
-
-            if (queue.Count > 0) {
-                GO.GetComponent<FollowTarget>().target = queue[queue.Count - 1];
-            } else {
-                GO.GetComponent<FollowTarget>().target = gameObject.transform;
-            }
-
+            //if (queue.Count > 0) {
+            //    GO.GetComponent<FollowTarget>().target = queue[queue.Count - 1];
+            //} else {
+            //    GO.GetComponent<FollowTarget>().target = gameObject.transform;
+            //}
             queue.Add(GO);
+            relinkQueue();
         }
+
+        public void RemoveFollower(Transform followerGO)
+        {
+            followerGO.GetComponent<FollowTarget>().target = null;
+            if (queue.Contains(followerGO)) {
+                queue.Remove(followerGO);
+            }
+            relinkQueue();
+        }
+
+        private void relinkQueue()
+        {
+            for (int i = 0; i < queue.Count; i++) {
+                Debug.Log("relinkQueue " + i);
+                if (i < 1) {
+                    queue[i].GetComponent<FollowTarget>().target = gameObject.transform;
+                } else {
+                    queue[i].GetComponent<FollowTarget>().target = queue[i - 1];
+                }
+            }
+        }
+
 
         public void EnterMaze()
         {
