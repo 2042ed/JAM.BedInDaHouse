f189321caa7a1142222cc6c5d1a132a5496b4ed8
Peterpunk <ppolsinelli@gmail.com>
2019-01-27 12:32:32 +0100

    no message

--- Assets/_game/_scripts/IntroManager.cs
+++ Assets/_game/_scripts/IntroManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IntroManager : MonoBehaviour
 {
@@ -15,11 +16,24 @@ public class IntroManager : MonoBehaviour
     [SerializeField]
     CanvasGroup Cover;
 
+    [SerializeField]
+    CanvasGroup Play;
+
+    [SerializeField]
+    RectTransform AllChars;
+
+    [SerializeField]
+    AudioClip CiroMumble;
+
+    [SerializeField]
+    AudioSource Audio;
+
     // Start is called before the first frame update
     void Start()
     {
         Team.alpha = 1;
         Title.alpha = 0;
+        Play.alpha = 0;
         Cover.alpha = 1;
         Cover.DOFade(0, 3);
 
@@ -29,7 +43,17 @@ public class IntroManager : MonoBehaviour
             {
                 Team.alpha = 0;
                 Title.alpha = 1;
-                Cover.DOFade(0, 2);
+                Cover.DOFade(0, 2).OnComplete(() =>
+                {
+                    Audio.clip = CiroMumble;
+                    Audio.Play();
+                    AllChars.DOAnchorPosX(750, 8).OnComplete(() => {  });
+                    foreach (var carat in AllChars.GetComponentsInChildren<Image>())
+                    {
+                        carat.rectTransform.DOAnchorPosY(20, 1).SetLoops(-1, LoopType.Yoyo);
+                    }
+                    Play.DOFade(1, 2);
+                });
             });
         });
     }
