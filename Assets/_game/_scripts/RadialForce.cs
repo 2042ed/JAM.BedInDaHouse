ab1c22ea6ac57ccb56369f6d7e9e01f2ff19ca93
Peterpunk <ppolsinelli@gmail.com>
2019-01-27 15:54:13 +0100

    no message

--- Assets/_game/_scripts/RadialForce.cs
+++ Assets/_game/_scripts/RadialForce.cs
@@ -4,36 +4,64 @@ using DG.Tweening;
 
 public class RadialForce : MonoBehaviour
 {
-    public BoxCollider2D PlayerColl;
-
     public float radius = 5.0F;
     public float power = 10.0F;
 
-    private bool pushing = false;
+    bool pushingSomeone;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !pushing) {
-            Debug.Log("SPACE");
-            var origSize = PlayerColl.size;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {            
+            if (pushingSomeone)
+                return;
 
-            pushing = true;
+            Collider2D[] colliders = Physics2D.OverlapAreaAll(new Vector2(transform.position.x,transform.position.y), 
+                new Vector2(1,1));
 
-            /*PlayerColl.size = new Vector2(3,3);
-            DOVirtual.DelayedCall(.5f, () =>
+
+            foreach (Collider2D hit in colliders)
             {
-                PlayerColl.size = origSize;
-                pushing = false;
-            });*/
+                if (hit.gameObject.tag == "Player")
+                    continue;
 
-            Vector3 explosionPos = transform.position;
-            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
-            foreach (Collider hit in colliders) {
+                Debug.Log("hit "+hit.name);
                 Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
 
-                if (rb != null) {
-                    rb.AddForce(transform.up);
+                if (rb != null)
+                {
+                    pushingSomeone = true;
+                    {
+                        var pushedX = rb.transform.position.x;
+                        var range = Random.Range(1.2f, 1.7f);
+
+                        if (transform.position.x < pushedX)
+                        {
+                            rb.transform.DOMoveX(pushedX + range, 0.5f).OnComplete(()=> pushingSomeone=false);
+                        }
+                        else
+                        {
+                            rb.transform.DOMoveX(pushedX - range, 0.5f).OnComplete(() => pushingSomeone = false);
+                        }
+                    }
+                    {
+                        var pushedY = rb.transform.position.y;
+                        var range = Random.Range(1.2f, 1.7f);
+
+                        if (transform.position.y < pushedY)
+                        {
+                            rb.transform.DOMoveY(pushedY + range, 0.5f).OnComplete(() => pushingSomeone = false);
+                        }
+                        else
+                        {
+                            rb.transform.DOMoveY(pushedY - range, 0.5f).OnComplete(() => pushingSomeone = false);
+                        }
+                    }
+
                 }
-            }*/
+
+                //if (rb != null)
+                //    rb.AddForce(new Vector2(Random.Range(0.2f,0.8f), Random.Range(0.2f, 0.8f)), ForceMode2D.Force);
+            }
         }
     }
 }
