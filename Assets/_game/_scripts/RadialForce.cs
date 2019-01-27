f5c76d80d6f368399ad07a8eb6a97228ac1311ee
Peterpunk <ppolsinelli@gmail.com>
2019-01-27 14:49:25 +0100

    no message

--- Assets/_game/_scripts/RadialForce.cs
+++ Assets/_game/_scripts/RadialForce.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class RadialForce : MonoBehaviour
 {
+    public BoxCollider2D PlayerColl;
+
     public float radius = 5.0F;
     public float power = 10.0F;
 
+    private bool pushing = false;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && !pushing) {
             Debug.Log("SPACE");
+            var origSize = PlayerColl.size;
+
+            pushing = true;
+
+            /*PlayerColl.size = new Vector2(3,3);
+            DOVirtual.DelayedCall(.5f, () =>
+            {
+                PlayerColl.size = origSize;
+                pushing = false;
+            });*/
+
             Vector3 explosionPos = transform.position;
             Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
             foreach (Collider hit in colliders) {
@@ -18,7 +33,7 @@ public class RadialForce : MonoBehaviour
                 if (rb != null) {
                     rb.AddForce(transform.up);
                 }
-            }
+            }*/
         }
     }
 }
